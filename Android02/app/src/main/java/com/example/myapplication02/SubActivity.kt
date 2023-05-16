6870352a9cec025dcc9e812303f8549c44965bea
hyuna <akxmcse@gmail.com>
2023-05-16 23:42:05 +0900

    [FEAT] 뒤로 버튼에 finish 추가

--- Android02/app/src/main/java/com/example/myapplication02/SubActivity.kt
+++ Android02/app/src/main/java/com/example/myapplication02/SubActivity.kt
@@ -1,19 +1,26 @@
 package com.example.myapplication02
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.myapplication02.databinding.ActivityMainBinding
+import com.example.myapplication02.databinding.ActivitySubBinding
 
 //import kotlinx.android.synthetic.main.activity_sub.*
 
 class SubActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySubBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sub)
+        binding = ActivitySubBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val backBtn = findViewById<Button>(R.id.moveBack)
-        //버튼 클릭시 액티비티 종료
-        backBtn.setOnClickListener {
+        setIntent()
+    }
+
+    private fun setIntent() {
+        binding.btnMoveBack.setOnClickListener{
             finish()
         }
     }
